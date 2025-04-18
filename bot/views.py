f139053a47552b15c1a6c1fe806de481b7d51f09
santosh12197 <santoshkumar12197@gmail.com>
2025-04-18 17:30:51 +0530

    started chatBot

---
+++ bot/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+
+
+
+class IndexView(View):
+
+    def get(self, request):
+
+        template_name = "index.html"
+
+        return render(request, template_name)
